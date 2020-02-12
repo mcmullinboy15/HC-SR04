3a8304618c18822b66bf3cb639f9376e496a7450
Andrew McMullin <mcmullinboy15@gmail.com>
2020-02-12 00:54:11 -0700

    edited run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
-git pull
+#git pull
 
-echo "Running script_setup.sh"
-source script_setup.sh
+#echo "if"
+#if [  ]
 
-echo "Running the sensor"
-python3 exe_ver_1.0.py
+total="$(wc -l < user_data.csv | tr -d ' ')"
+echo "$total"
+
+
+#echo "Running script_setup.sh"
+#source script_setup.sh
+
+#echo "Running the sensor"
+#python3 exe_ver_1.0.py
