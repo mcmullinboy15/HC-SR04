134191e28f8bacaa6946bfc325b91f5f1a21db1f
Andrew <mcmullinboy15@gmail.com>
2020-02-20 02:39:21 +0000

    I'm closer but I still need to do:   SSL, wifi_setup.py, and crontab

--- script_setup.sh
+++ script_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+SECONDS=0
 
 if [ $(source setup_complete.sh) = 'true' ]
 then
@@ -30,18 +31,26 @@ else
 # see if the updats above get python3 working
   echo "upgrading python"
   sudo apt-get update
+  sleep 5
   sudo apt-get install
+  sleep 5
   echo "Y" | sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.4-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+  #sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+  
+  sleep 10
 
   wget https://www.python.org/ftp/python/3.7.6/Python-3.7.6.tgz
   tar -zxvf Python-3.7.6.tgz
   cd Python-3.7.6
   ./configure
   make
+  sleep 5
   sudo make altinstall
 
-  sudo rm -r Python-3.7.6
-  rm Python-3.7.6.tar.xz
+  sleep 10 
+
+  sudo rm -rf Python-3.7.6
+  rm -rf Python-3.7.6.tar.xz
   sudo apt-get --purge remove build-essential tk-dev
   sudo apt-get --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev
   sudo apt-get --purge remove libdb5.4-dev libgdbm-dev libsqlite3-dev libssl-dev
@@ -49,13 +58,15 @@ else
   sudo apt-get autoremove
   sudo apt-get clean
 
+  sleep 10
+
   cd
   echo "alias gs='git status'" >> .bash_aliases
   echo "alias python='python3.7'" >> .bash_aliases
   echo "alias python3='python3.7'" >> .bash_aliases
   echo "alias pip='pip3.7'" >> .bash_aliases
   echo "alias pip3='pip3.7'" >> .bash_aliases
-
+  . ~/.bashrc 
 
 
   path="$HOME/Documents/EZ_Salt/HC-SR04"
@@ -76,11 +87,17 @@ else
     cd "$HOME"/Documents/EZ_Salt/HC-SR04/ || return
   fi
 
+  sleep 10
+
   echo "setting up wifi"
   sudo rfkill unblock 0
   sudo ifconfig wlan0 up
+  sleep 5
   python3 wifi_setup.py
+  sleep 5
   wpa_cli -i wlan0 reconfigure
+  
+  sleep 20
 
   echo
   echo "changing terminal design temporarily"
@@ -97,8 +114,18 @@ else
   pip install smtplib
   echo "not going to instal 'apt-get install python-pandas'"
   #sudo apt-get install python-pandas
+  
+  sleep 5
 
   echo "when finished I'll change the -1 to 1"
   source setup_complete.sh
+  
+  sleep 5
 fi
 echo
+
+
+
+
+duration=$SECONDS
+echo "Script_setup.sh Ran in $(($duration / 60)) minutes and $(($duration % 60)) seconds."
