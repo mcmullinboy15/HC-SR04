1a6da33cce39e08490c8445378922528b8b1bead
Andrew McMullin <48926044+mcmullinboy15@users.noreply.github.com>
2020-06-22 20:25:37 -0600

    Update email__.py

--- email__.py
+++ email__.py
@@ -17,6 +17,13 @@ https://www.nexmo.com/blog/2019/03/21/sending-sms-from-python-with-google-cloud-
 
 class Email():
     """ Sending Multiple Messages """
+    """ 
+    Things to do to get this to work: 
+        
+     - Visit https://myaccount.google.com/lesssecureapps to setup your  
+        account to allow less secure app access such as this one
+        
+    """
     """ Received from: [ https://nitratine.net/blog/post/how-to-send-an-email-with-python/ ] """
 
     def __init__(self):
